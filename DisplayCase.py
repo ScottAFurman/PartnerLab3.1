766b263ffb95266d4c22798083d1ad174cd28c6e
Cmonboy <36928499+Cmonboy@users.noreply.github.com>
2018-03-07 10:47:59 -0600

    Add files via upload

--- DisplayCase.py
+++ DisplayCase.py
@@ -2,3 +2,9 @@
 # Authors: Scott Furman and Adrian Michniewicz
 # Class: Programming Two - Period 04
 # Last Modified: March 7th, 2018
+
+from updatedGraphics import *
+class DisplayCase(object):
+    def __init__(self,win):
+        book=Book(,,"",win)
+        comic=ComicBook(,,"",win)
