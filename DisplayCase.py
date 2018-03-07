48a736c02edbcd23acf722b8b4b60a49142f780d
Cmonboy <36928499+Cmonboy@users.noreply.github.com>
2018-03-07 11:02:32 -0600

    Add files via upload

--- DisplayCase.py
+++ DisplayCase.py
@@ -4,6 +4,13 @@
 # Last Modified: March 7th, 2018
 
 from updatedGraphics import *
+def drawCase(self,win):
+    rec=Rectangle(Point(275,600),Point(725,200))
+    sign=Rectangle(Point(275,425),Point(725,525))
+    words=Text(Point(450,450))
+
+
+
 class DisplayCase(object):
     def __init__(self,win):
         book=Book(,,"",win)
